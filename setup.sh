5e6ca0e925107c8f84601f8316f20340b3c11b00
Sable Evans <brodyevans82@gmail.com>
2026-01-19 20:21:07 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+apt-get update
+apt-get upgrade -y
+
+
+# setup screen
+
+#setup GPOI for encoder
+
+#install all necessary packages
+
+#we need to decide if we're going to use python or C++ for this project. if we use C++ do we have them compile it locally? and if we compile locally, do we add a loading screeen?
+
+echo "The system will now reboot."
+sudo reboot now
